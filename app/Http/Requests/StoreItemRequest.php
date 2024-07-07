86a3a79b75d202bee6343d7b93af0288f8d69232
Imtyaz Ahmed <54882826+imtyaz-17@users.noreply.github.com>
2024-07-07 16:10:07 +0600

    Update StoreItemRequest.php

--- app/Http/Requests/StoreItemRequest.php
+++ app/Http/Requests/StoreItemRequest.php
@@ -1,3 +1,5 @@
+<?php
+
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
@@ -6,10 +8,8 @@ class StoreItemRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
-     *
-     * @return bool
      */
-    public function authorize()
+    public function authorize(): bool
     {
         return true;
     }
@@ -17,7 +17,7 @@ class StoreItemRequest extends FormRequest
     /**
      * Get the validation rules that apply to the request.
      *
-     * @return array
+     * @return array<string, \Illuminate\Contracts\Validation\Rule|array|string>
      */
     public function rules(): array
     {
