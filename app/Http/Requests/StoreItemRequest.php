ae9ce02912cbbc93cab3a7e7bf19e8850a2f6e45
Imtyaz Ahmed <54882826+imtyaz-17@users.noreply.github.com>
2024-07-07 16:14:32 +0600

    T6 Update StoreItemRequest.php

--- app/Http/Requests/StoreItemRequest.php
+++ app/Http/Requests/StoreItemRequest.php
@@ -8,8 +8,10 @@ class StoreItemRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
+     *
+     * @return bool
      */
-    public function authorize(): bool
+    public function authorize()
     {
         return true;
     }
@@ -17,13 +19,13 @@ class StoreItemRequest extends FormRequest
     /**
      * Get the validation rules that apply to the request.
      *
-     * @return array<string, \Illuminate\Contracts\Validation\Rule|array|string>
+     * @return array
      */
-    public function rules(): array
+    public function rules()
     {
         return [
-            'name' => ['required'],
-            'description' => ['required']
+            'name' => 'required',
+            'description' => 'required',
         ];
     }
 }
